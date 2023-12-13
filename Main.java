c58f6f6eb2da245ac2af3c93bd391c17cde700f4
MandichRiccardo <ricky.mandich@gmail.com>
2023-12-13 23:06:07 +0100

    creazione 2023 12 13 23:00

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String args[]){
+        String s1 = "banana";
+        String s2 = "mango";
+        
+        s1.indexOf('a');
+        s1.lastIndexOf('a');
+        s1.indexOf("na");
+        s1.concat(s2);
+        
+    }
+}
